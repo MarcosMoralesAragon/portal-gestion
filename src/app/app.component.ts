e4cc114e50743a9c3ba2942d5fad6840a2311940
MarcosMoralesAragon <marcosmoralesaragon@gmail.com>
2022-02-14 11:46:48 +0100

    Creación de toast a la hora de cargar los ficheros

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
+import { SnackbarComponent } from './components/snackbar/snackbar.component';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +10,57 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'portal-gestion';
-  constructor(private router:Router){}
+  fileName = ""
+
+  constructor(private router:Router,
+              private http:HttpClient,
+              private _snackBar: MatSnackBar){}
+
+  onFileSelected(event : any) {
+
+    const file:File = event.target.files[0];
+
+    if (file) {
+        this.fileName = file.name;
+        console.log(this.fileName)
+        this.showSnackBar("updateFile", "Archivo (" + this.fileName +  ") subido correctamente")
+        // const formData = new FormData();
+        // formData.append("thumbnail", file);
+        // const upload$ = this.http.post("/api/thumbnail-upload", formData);
+        // upload$.subscribe();
+    } else {
+      this.showSnackBarError("updateFile", "Subida de archivo cancelada")
+    }
+  }
+
+  showSnackBar(acctionDone:string, message: string){
+    this._snackBar.openFromComponent(SnackbarComponent, {
+      duration : 3250,
+      panelClass: ['green-snackbar'],
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      
+      data:{
+        messageSnackbar: message,
+        acctionDoneSnackbar : acctionDone
+      } 
+    })
+  }
+
+  showSnackBarError(acctionDone:string, message: string){
+    this._snackBar.openFromComponent(SnackbarComponent, {
+      duration : 3250,
+      panelClass: ['red-snackbar'],
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+      data:{
+        messageSnackbar: message,
+        acctionDoneSnackbar : acctionDone
+      } 
+    })
+  }
 
   goToList(){
     this.router.navigateByUrl('/')
