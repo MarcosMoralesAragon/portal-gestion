fc8f3b45173b7c11ddd3340416384448fd1a0c38
MarcosMoralesAragon <marcosmoralesaragon@gmail.com>
2022-02-14 10:57:58 +0100

    Nuevo boton para subir archivos, todavía no hace nada con el archivo subido

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -7,8 +8,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'portal-gestion';
-  constructor(private router:Router){}
+  fileName = ""
+
+  constructor(private router:Router,
+              private http:HttpClient){}
+
+  onFileSelected(event : any) {
+
+    const file:File = event.target.files[0];
+
+    if (file) {
+        this.fileName = file.name;
+        console.log(this.fileName)
+        // const formData = new FormData();
+        // formData.append("thumbnail", file);
+        // const upload$ = this.http.post("/api/thumbnail-upload", formData);
+        // upload$.subscribe();
+    }
+}
 
   goToList(){
     this.router.navigateByUrl('/')
