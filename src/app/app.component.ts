7545442b877c805a5593d632f46dd714d34f00b6
MarcosMoralesAragon <marcosmoralesaragon@gmail.com>
2022-02-15 14:35:35 +0100

    Falta que mande el json con la petición que corresponda al backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,8 @@ import { Component } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { Router } from '@angular/router';
 import { SnackbarComponent } from './components/snackbar/snackbar.component';
+import { FileService } from './services/file/file.service';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-root',
@@ -12,25 +14,55 @@ import { SnackbarComponent } from './components/snackbar/snackbar.component';
 export class AppComponent {
 
   title = 'portal-gestion';
-  fileName = ""
 
   constructor(private router:Router,
               private http:HttpClient,
-              private _snackBar: MatSnackBar){}
+              private _snackBar: MatSnackBar,
+              private fileService : FileService){}
   
   onFileSelected(event : any) {
 
+    // Saca el archivo introducido
     const file:File = event.target.files[0];
 
     if (file) {
-        this.fileName = file.name;
-        console.log(this.fileName)
-        this.showSnackBar("updateFile", "Archivo (" + this.fileName +  ") subido correctamente")
+      if(this.getFileExtension(file.name) === "xlsx"){
 
-        // const formData = new FormData();
-        // formData.append("thumbnail", file);
-        // const upload$ = this.http.post("/api/thumbnail-upload", formData);
-        // upload$.subscribe();
+        // Objeto que nos permite leer el archivo
+        const fileReader = new FileReader();
+        fileReader.readAsBinaryString(file)
+
+        fileReader.onload = (event:any) => {
+          // Saca los datos y lo vuelve un binario
+          let binaryData = event.target.result
+          // Crea un objeto con todos los datos del xlsx que le hemos pasado
+          let workbook = XLSX.read(binaryData,{type:'binary'})
+
+          // Por cada hoja del xlsx lo combierte a un json los datos de las columnas
+          workbook.SheetNames.forEach(sheet => {
+            // Hace json la hoja sheet
+            const data = XLSX.utils.sheet_to_json(workbook.Sheets[sheet])
+
+            switch (sheet) {
+              case "Empleado":
+                console.log("Esta en empleado")
+                break;
+              case "Contrato":
+                console.log("Esta en contrato")
+                break;
+              case "Dirección":
+                console.log("Esta en dirección")
+              break;
+              default:
+                break;
+            }
+            console.log(data)
+          })
+          console.log(workbook)
+        }
+      } else {
+        this.showSnackBarError("updateFile", "Formato de archivo no admitido. Tiene que ser xlsx")
+      }
     } else {
       this.showSnackBarError("updateFile", "Subida de archivo cancelada")
     }
@@ -63,6 +95,9 @@ export class AppComponent {
     })
   }
 
+  getFileExtension(fileName : string) : string{
+    return fileName.split('.')[1]
+  }
 
   goToList(){
     this.router.navigateByUrl('/')
